683010de51cba6f27c5c9f9a6d09400674a9852c
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-12-17 18:24:19 +0100

    [add] feat end milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
-
+import { useState, useEffect } from "react"
 
 function App() {
 
+  const [query, setQuery] = useState('')
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async (query) => {
+    if (!query.trim()) {
+      setProducts([])
+      return;
+    }
+    try {
+      const res = await fetch(`http://localhost:3333/products?search=${query}`)
+      const data = await res.json()
+      setProducts(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts(query)
+  }, [query])
+
+  console.log(products)
+  console.log(query)
+
   return (
     <>
-
+      <div>
+        <input type="text" placeholder="Cerca..." value={query} onChange={e => setQuery(e.target.value)} />
+        {products.length > 0 && (
+          <div className="dropdown">
+            {products.map(product => {
+              return (
+                <p key={product.id}>{product.name}</p>
+              )
+            })}
+          </div>
+        )}
+      </div>
     </>
   )
 }
